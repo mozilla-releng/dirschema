4aeca5e305c8b54edaa7fdefe944ef54cd044a5c
Ben Hearsum <bhearsum@mozilla.com>
2019-11-01 15:12:01 -0400

    Implement basic github repo checking.

--- src/dirschema/checks.py
+++ src/dirschema/checks.py
@@ -1,41 +1,29 @@
 import logging
+from base64 import b64decode
 from pathlib import Path
 
-logger = logging.getLogger(__file__)
+from github import Github
+
+logger = logging.getLogger("dirschema")
 
 
-def check_file(schema, file_):
-    if not schema.get("contains"):
-        return []
-
+def check_file(schema, filename, contents):
     errors = []
-    contents = open(file_).read()
 
     for required in schema.get("contains", []):
         if required not in contents:
-            errors.append(f"{file_.name} is missing required string: '{required}'")
+            errors.append(f"{filename} is missing required string: '{required}'")
 
     return errors
 
 
-def check_dir(schema, dir_, dirname=None):
-    if not dirname:
-        dirname = dir_.name
+def check_dir(schema, dirname, found_files, found_dirs):
     allow_extra_files = schema.get("allow_extra_files")
     allow_extra_dirs = schema.get("allow_extra_dirs")
     expected_files = set(schema.get("files", []))
     expected_dirs = set(schema.get("dirs", []))
-    found_files = set()
-    found_dirs = set()
     errors = []
 
-    for i in dir_.iterdir():
-        if i.is_file():
-            found_files.add(i.name)
-        if i.is_dir():
-            found_dirs.add(i.name)
-
-    # Missing files & dirs
     for f in expected_files - found_files:
         errors.append(f"missing file in {dirname}: {f}")
     for d in expected_dirs - found_dirs:
@@ -48,13 +36,40 @@ def check_dir(schema, dir_, dirname=None):
         for d in found_dirs - expected_dirs:
             errors.append(f"extra dir in {dirname}: {d}")
 
+    return errors
+
+
+def check_ondisk_file(schema, file_):
+    if not schema.get("contains"):
+        return []
+
+    contents = open(file_).read()
+    return check_file(schema, file_.name, contents)
+
+
+def check_ondisk_dir(schema, dir_, dirname=None):
+    if not dirname:
+        dirname = dir_.name
+
+    found_files = set()
+    found_dirs = set()
+    errors = []
+
+    for i in dir_.iterdir():
+        if i.is_file():
+            found_files.add(i.name)
+        if i.is_dir():
+            found_dirs.add(i.name)
+
+    errors.extend(check_dir(schema, dirname, found_files, found_dirs))
+
     for d in found_dirs:
         if d in schema.get("dirs", {}):
-            errors.extend(check_dir(schema["dirs"][d], dir_ / d))
+            errors.extend(check_ondisk_dir(schema["dirs"][d], dir_ / d))
 
     for f in found_files:
         if f in schema.get("files", {}):
-            errors.extend(check_file(schema["files"][f], dir_ / f))
+            errors.extend(check_ondisk_file(schema["files"][f], dir_ / f))
 
     return errors
 
@@ -65,8 +80,57 @@ def check_ondisk_structure(schema, rootdir):
     if not rootdir.is_dir():
         return [f"invalid directory: {rootdir.name}"]
 
-    return check_dir(schema, rootdir, "root directory")
+    return check_ondisk_dir(schema, rootdir, "root directory")
 
 
-def check_github_structure(schema, uri):
-    return
+def check_github_file(schema, repo, file_):
+    logger.debug(f"Verifying file: {file_}")
+
+    if not schema.get("contains"):
+        logger.debug(f"Schema has no content requirements, not downloading file")
+        return []
+
+    # TODO: Casting to a string is probably not the right fix for this.
+    # Maybe we should be reading the schema as UTF-8?
+    contents = str(b64decode(repo.get_contents(file_).content))
+    logger.debug(f"Got contents:")
+    logger.debug(contents)
+    return check_file(schema, file_, contents)
+
+
+def check_github_dir(schema, repo, dir_, dirname=None):
+    if not dirname:
+        dirname = dir_
+
+    found_files = set()
+    found_dirs = set()
+    errors = []
+
+    logger.debug(f"Processing dir: {dirname}")
+    for i in repo.get_contents(dir_):
+        if i.type == "file":
+            logger.debug(f"Found file: {i.name}")
+            found_files.add(i.name)
+        elif i.type == "dir":
+            logger.debug(f"Found dir: {i.path}")
+            found_dirs.add(i.path)
+
+    errors.extend(check_dir(schema, dirname, found_files, found_dirs))
+
+    for d in found_dirs:
+        if d in schema.get("dirs", {}):
+            errors.extend(check_github_dir(schema["dirs"][d], repo, d))
+
+    for f in found_files:
+        if f in schema.get("files", {}):
+            errors.extend(check_github_file(schema["files"][f], repo, f))
+
+    return errors
+
+
+def check_github_structure(schema, repo_name, access_token):
+    # TODO: error handling
+    g = Github(access_token)
+    repo = g.get_repo(repo_name)
+
+    return check_github_dir(schema, repo, "", "root directory")
--- src/dirschema/cli.py
+++ src/dirschema/cli.py
@@ -1,20 +1,35 @@
+import logging
+import logging.config
 import sys
+from urllib.parse import urlparse
 
 import click
-
-from .checks import check_github_structure, check_ondisk_structure
-from .schema import load_schema
 
 
 @click.command()
 @click.argument("schema")
 @click.argument("project_dir_or_repo")
-def dirschema(schema, project_dir_or_repo):
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
+@click.option("--access-token", help="Github access token, if checking a repository")
+def dirschema(schema, project_dir_or_repo, verbose, access_token):
+    # TODO: why isn't logging.config.dictConfig working when we
+    # set config for a "dirschema" logger
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger("github").setLevel(logging.WARNING)
+        logging.getLogger("urllib3").setLevel(logging.WARNING)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    from .checks import check_github_structure, check_ondisk_structure
+    from .schema import load_schema
+
     schema = load_schema(open(schema).read())
 
     # Surely this assumption will never break...
     if "://" in project_dir_or_repo:
-        errors = check_github_structure(schema, project_dir_or_repo)
+        repo = urlparse(project_dir_or_repo).path[1:]
+        errors = check_github_structure(schema, repo, access_token)
     else:
         errors = check_ondisk_structure(schema, project_dir_or_repo)
 
