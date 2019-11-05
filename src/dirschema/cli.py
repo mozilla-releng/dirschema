dbfef56e57c2b7e5ad8b949c19067e951a68d86a
Ben Hearsum <bhearsum@mozilla.com>
2019-11-05 16:01:21 -0500

    Add support for verifying multiple projects in one invocation.

--- src/dirschema/cli.py
+++ src/dirschema/cli.py
@@ -9,7 +9,7 @@ import click
 
 @click.command()
 @click.argument("schema")
-@click.argument("project_dir_or_repo")
+@click.argument("project_dir_or_repo", nargs=-1)
 @click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
 @click.option(
     "--access-token",
@@ -31,16 +31,27 @@ def dirschema(schema, project_dir_or_repo, verbose, access_token):
 
     schema = load_schema(open(schema).read())
 
-    # Surely this assumption will never break...
-    if "://" in project_dir_or_repo:
-        repo = urlparse(project_dir_or_repo).path[1:]
-        errors = check_github_structure(schema, repo, access_token)
-    else:
-        errors = check_ondisk_structure(schema, project_dir_or_repo)
+    project_errors = {}
 
-    if errors:
-        click.echo("\n".join(errors))
+    for tocheck in project_dir_or_repo:
+        print(tocheck)
+        # Surely this assumption will never break...
+        if "://" in tocheck:
+            repo = urlparse(tocheck).path[1:]
+            project_errors[tocheck] = check_github_structure(schema, repo, access_token)
+        else:
+            project_errors[tocheck] = check_ondisk_structure(schema, tocheck)
+
+    any_errors = False
+    for project, errors in project_errors.items():
+        if errors:
+            any_errors = True
+            click.echo(f"{project}: Has errors")
+            click.echo("\n".join(errors))
+        else:
+            click.echo(f"{project}: Success!")
+
+    if any_errors:
         sys.exit(1)
     else:
-        click.echo("Success!")
         sys.exit(0)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,6 +21,8 @@ def test_cli_good_project(make_project, tmp_path):
     if result.exit_code != 0:
         assert False, print_exception(*result.exc_info[:3])
 
+    assert result.stdout.count("Success!") == 1
+
 
 def test_cli_bad_project(make_project, tmp_path):
     make_project(tmp_path, BAD_PROJECT)
@@ -30,3 +32,36 @@ def test_cli_bad_project(make_project, tmp_path):
 
     if result.exit_code != 1:
         assert False, print_exception(*result.exc_info[:3])
+
+    assert result.stdout.count("missing file in root directory") == 2
+
+
+def test_cli_multiple_good_projects(make_project, tmp_path_factory):
+    dirs = [tmp_path_factory.mktemp("d1"), tmp_path_factory.mktemp("d2")]
+
+    for d in dirs:
+        make_project(d, GOOD_PROJECT)
+
+    runner = CliRunner()
+    result = runner.invoke(dirschema, [str(SCHEMA_PATH), *[str(d) for d in dirs]])
+
+    if result.exit_code != 0:
+        assert False, print_exception(*result.exc_info[:3])
+
+    assert result.stdout.count("Success!") == 2
+
+
+def test_cli_multiple_projects_one_bad(make_project, tmp_path_factory):
+    good_dir, bad_dir = [tmp_path_factory.mktemp("d1"), tmp_path_factory.mktemp("d2")]
+
+    make_project(good_dir, GOOD_PROJECT)
+    make_project(bad_dir, BAD_PROJECT)
+
+    runner = CliRunner()
+    result = runner.invoke(dirschema, [str(SCHEMA_PATH), str(good_dir), str(bad_dir)])
+
+    if result.exit_code != 1:
+        assert False, print_exception(*result.exc_info[:3])
+
+    assert result.stdout.count("Success!") == 1
+    assert result.stdout.count("missing file in root directory") == 2
